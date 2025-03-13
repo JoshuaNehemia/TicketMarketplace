75c3171ebf1a4e04924a5c377ba216c14c8b2421
franly2 <franlybudipramana588@gmail.com>
2025-03-13 16:51:29 +0700

    fixing db connection

--- Ticket_Marketplace/src/ticket_marketplace/Database_Connection.java
+++ Ticket_Marketplace/src/ticket_marketplace/Database_Connection.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 public class Database_Connection {
     private static final String URL = "jdbc:mysql://localhost:3306/projectjava1";
     private static final String USER = "root"; 
-    private static final String PASSWORD = "ganairsm";
+    private static final String PASSWORD = "";
     
     public static Connection getConnection() {
         try {
