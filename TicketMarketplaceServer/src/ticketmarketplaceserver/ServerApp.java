53161a039d64f6ecaf3b32fc00bbcaeb541e3102
JoshuaNs <joshuans224@gmail.com>
2025-04-08 19:02:06 +0700

    Add ServerApp listening

--- TicketMarketplaceServer/src/ticketmarketplaceserver/ServerApp.java
+++ TicketMarketplaceServer/src/ticketmarketplaceserver/ServerApp.java
@@ -4,7 +4,8 @@
  */
 package ticketmarketplaceserver;
 
-import java.util.Scanner;
+import java.time.LocalDate;
+import Communication.*;
 
 
 /**
@@ -27,10 +28,21 @@ public class ServerApp {
     {
         System.out.println("Server is Running");
         service = new ServerService();
-        
-        //For Debugging
-        //while(true)
+        TCPManager tcp = new TCPManager(6000);
+        while(true)
         {
+            //Server accepting message from client
+            tcp.ReceivedCommunication();
+            String[] command = Communication.TranslateToListOfCommand(tcp.getCommunicationFromClient(), ";");
+            if(command[0].equals("LI"))
+            {
+                service.UserLogIn(command[1], command[2]);
+            }
+            else if(command[1].equals("SU"))
+            {
+                service.UserSignUp(command[1], command[2],command[3],command[4],LocalDate.parse(command[5]));
+            }
+            //For Debugging
             //MenuCLI.StartUpMenuCLI();
         }
     }
